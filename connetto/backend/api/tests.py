d2e033478cca781335cfbe08bd42c1bea80242b5
mao-sk <mkage.biz.ehim12@gmail.com>
2024-12-14 18:51:25 +0900

    add: firebase設定

--- connetto/backend/api/tests.py
+++ connetto/backend/api/tests.py
@@ -1,3 +1,14 @@
 # from django.test import TestCase
 
 # Create your tests here.
+import os
+
+from dotenv import load_dotenv
+
+# .envファイルの読み込み
+load_dotenv()
+
+# 環境変数の確認
+firebase_credentials_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+print(f"FIREBASE_CREDENTIALS_PATH: {firebase_credentials_path}")
+print("Firebase Admin SDK 初期化成功")
